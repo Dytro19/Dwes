17dab17cdf2f55e9c6cd69261ebe8a79a2e2d50f
Alvaro Gomez <alvagomuri@gmail.com>
2025-01-14 11:52:23 +0100

    sdjkvb

--- laragon/www/inventario/vistas/login.php
+++ laragon/www/inventario/vistas/login.php
@@ -1,6 +1,6 @@
 <div class="main-container">
 
-    <form class="box login" action="" method="POST" autocomplete="off">
+	<form class="box login" action="" method="POST" autocomplete="off">
 		<h5 class="title is-5 has-text-centered is-uppercase">Sistema de inventario</h5>
 
 		<div class="field">
@@ -11,7 +11,7 @@
 		</div>
 
 		<div class="field">
-		  	<label class="label">Contraseña</label>
+		  	<label class="label">Clave</label>
 		  	<div class="control">
 		    	<input class="input" type="password" name="login_clave" pattern="[a-zA-Z0-9$@.-]{7,100}" maxlength="100" required >
 		  	</div>
@@ -20,6 +20,14 @@
 		<p class="has-text-centered mb-4 mt-3">
 			<button type="submit" class="button is-info is-rounded">Iniciar sesion</button>
 		</p>
+
+		<?php
+			if(isset($_POST['login_usuario']) && isset($_POST['login_clave'])){
+				require_once "./php/main.php";
+				require_once "./php/iniciar_sesion.php";
+			}
+		?>
 	</form>
+
 
 </div>
